74db3711f9f525fa6eb7e1dbd607e67240f60a24
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-10-29 12:26:42 -0400

    Create function headers

---
+++ commands.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Consumes an array of file names and outputs the number of lines in them.
+void nl(char *fileNames[]);
+
+// Consumes a file name and outputs its content to the terminal
+void cat(char *fileName);
+
+// Consumes a file name and copies it to a new file
+void cp(char *fileName, char *newFileName);
+
+// Outputs the list of files at the current directory.
+void ls(); 
