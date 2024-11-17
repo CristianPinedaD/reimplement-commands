d91424ec78785d8ee1d29c46048e4d5c98374160
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2024-11-17 18:47:48 -0500

    add io

--- main.c
+++ main.c
@@ -4,18 +4,27 @@
 
 int main(int argc, char* argv[]) {
 
-    // Term should look like ./a.out . 
-	DIR *myDir = opendir(argv[1]);
+    // // Term should look like ./a.out . 
+	// DIR *myDir = opendir(argv[1]);
 
-	// opendir returns NULL on error, so we handle the error...
-	if (myDir == NULL) {
-		perror("Error while trying to open directory\n");
-		return 1;
+	// // opendir returns NULL on error, so we handle the error...
+	// if (myDir == NULL) {
+	// 	perror("Error while trying to open directory\n");
+	// 	return 1;
+	// }
+
+	// ls(myDir);
+
+	// closedir(myDir);
+
+	// cat("test.txt");
+
+	char command[200]; 
+	char argument[200]; 
+
+	fscanf("user@computer$",&command, &argument);
+
+	if (command == "ls") {
+		
 	}
-
-	ls(myDir);
-
-	closedir(myDir);
-
-	cat("test.txt");
 }
